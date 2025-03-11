adfbfe9bb35eb2814294cc55234670b31b71c3f0
Ben Coughlin <bencoughlin27@gmail.com>
2025-03-11 13:12:49 -0600

    added a basic text reader with error handling in the TextReader.java class. I also added the text file data to the repo. The data from the text currently goes into an arraylist of String[], which can be accessed with teh getInfo() method.

---
+++ SIS/src/TextReader.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TextReader {
+	
+	
+	public static void runFileReader()
+	{
+		/// TODO: temporary String arraylist until we get an object
+		ArrayList<String[]> studentInfo = new ArrayList<String[]>();
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader(new File("StudentInfo.txt")));
+			String line = br.readLine();
+			
+			while(line != null)
+			{
+				
+				studentInfo.add(line.split(" "));
+			}
+	
+		}
+		catch(IOException e)
+		{
+			System.out.println("Whoops, an error occured reading the file.");
+		}
+	}
+}
