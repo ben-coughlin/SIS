08ea3cd9eebfce75ccbabcc5ac385d960aeeeb78
tanner.vessely <tanner.vessely@MU-C1-10.mullen.pvt.k12.co.us>
2025-03-24 10:35:28 -0600

    coomints

---
+++ SIS/src/AddOrDeleteStudent.java
@@ -0,0 +1,24 @@
+
+public class AddOrDeleteStudent
+	{
+//add student
+		
+		 public void addStudent(String firstName, String lastName, String[] grades) {
+		        Student newStudent = new Student(, , , , );
+		        studentList.add(newStudent);
+		        System.out.println("Student " + firstName + " " + lastName + " has been added.");
+		    }
+	
+		//delete student
+		
+	
+		 public void deleteStudent(String firstName, String lastName) {
+		   
+		        for (Student student : students) {
+		            if (student.firstName.equals(firstName) && student.lastName.equals(lastName)) {
+		               
+		       
+		            }
+		        }
+		 }
+		 }
