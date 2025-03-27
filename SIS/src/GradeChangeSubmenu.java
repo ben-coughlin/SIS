f0fbdde6e63afa287a0da5194d8480e2b9cc74ef
jacqueline.magolske <jacqueline.magolske@MU-C1-13.mullen.pvt.k12.co.us>
2025-03-27 12:39:31 -0600

    changed made to GradeChange and Schedule Change submenus

--- SIS/src/GradeChangeSubmenu.java
+++ SIS/src/GradeChangeSubmenu.java
@@ -7,16 +7,37 @@ public class GradeChangeSubmenu
 		public static void GradeIsChanging()
 			{
 				
-				ArrayList<Student> gradeChanged = TextReader.getStudentInfo();
-				
+				ArrayList <Student> gradeChanged = TextReader.getStudentInfo();
 				Scanner userInput = new Scanner (System.in);
 				
+				
 				System.out.println();
-				System.out.println("You have chosen to switch a students grade.");
+				System.out.println("You have chosen to switch a student's grade.");
 				System.out.println("What student do you want to work with?");
+				System.out.println();
 				
+				for(int i = 0; i < gradeChanged.size(); i++)
+					{
+						System.out.println(gradeChanged.get(i).getFirstName());
+					}
 				
-				int studentChosen = userInput.nextInt();
+				System.out.println();
+				
+				String studentChosen = userInput.nextLine();
+				System.out.println("Alright! You chose to change " + studentChosen + "'s grade.");
+				System.out.println("Finding student...");
+				System.out.println();
+				
+				for(int i = 0; i < gradeChanged.size(); i++)
+					{
+						if(gradeChanged.get(i).getFirstName().equals(studentChosen))
+							{
+								System.out.println("Found!");
+								
+								
+							}
+					}
+				
 
 			}
 
--- SIS/src/ScheduleChangeSubmenu.java
+++ SIS/src/ScheduleChangeSubmenu.java
@@ -7,15 +7,60 @@ public class ScheduleChangeSubmenu
 		public static void ScheduleIsChanging()
 			{
 				
-				ArrayList<Student> scheduleChanged = TextReader.getStudentInfo();
+				ArrayList <Student> scheduleChanged = TextReader.getStudentInfo();
+				Scanner userInput = new Scanner (System.in);
+				
 				
 				System.out.println();
 				System.out.println("You have chosen to switch a student's class.");
 				System.out.println("What student do you want to work with?");
+				System.out.println();
 				
-				Scanner userInput = new Scanner (System.in);
-				int studentChosen = userInput.nextInt();
+				for(int i = 0; i < scheduleChanged.size(); i++)
+					{
+						System.out.println(scheduleChanged.get(i).getFirstName());
+					}
+				
+				System.out.println();
+				
+				String studentChosen = userInput.nextLine();
+				System.out.println("Alright! You chose to change " + studentChosen + "'s class.");
+				System.out.println("Finding student...");
+				System.out.println();
+				
+				for(int i = 0; i < scheduleChanged.size(); i++)
+					{
+						if(scheduleChanged.get(i).getFirstName().equals(studentChosen))
+							{
+								System.out.println("Found!");
+								
+								
+							}
+					}
+				
 
 			}
 
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
